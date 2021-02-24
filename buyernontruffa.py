814f33facf8a0837015880648f56b28a36cba34d
Pietro Zarri <pietrozarri96@gmail.com>
2021-02-24 19:33:49 +0100

    fix turbo button

--- buyernontruffa.py
+++ buyernontruffa.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
 import os
 import time
 import secrets
@@ -11,17 +8,23 @@ import secrets
 options = Options()
 options.page_load_strategy = 'eager'
 
-login_url = secrets.login_url
 
-def purchase(username, password, item_url, cvv):
+def purchase(username, password, item_url, cvv, country):
+    if country == "Amazon.it":
+        login_url = secrets.login_url_it
+    elif country == "Amazon.es":
+        login_url = secrets.login_url_es
+    elif country == "Amazon.fr":
+        login_url = secrets.login_url_fr
+    elif country == "Amazon.de":
+        login_url = secrets.login_url_de
+    else:
+        raise Exception("Error in link")
+
     address_xpath = '//*[@id="address-book-entry-0"]/div[2]/span/a'
-    # if its your second address in your amazon account use this address xpath='//*[@id="address-book-entry-1"]/div[2]/span/a'
-    # in simple put (n-1) value in place of //*[@id="address-book-entry-___________________"]/div[2]/span/a where n is address number you want to deliver from your amazon account
     driver = webdriver.Chrome(os.getcwd()+"/chromedriver", options=options)
-    wait = WebDriverWait(driver, 10)
 
    # for logging into amazon
-
     driver.get(login_url)
     driver.find_element_by_xpath(
         '//*[@id="ap_email"]').send_keys(username + Keys.RETURN)
@@ -32,32 +35,15 @@ def purchase(username, password, item_url, cvv):
     time.sleep(0.1)
     # for getting into our product page
     driver.get(item_url)
-  
-    # refresh till we find buy now button in amazon
-    #while not driver.find_elements_by_xpath('//*[@id="buy-now-button"]'):
-    #    driver.refresh()
+
     # once the button is activated ,click the buy now button
     driver.find_element_by_xpath(
         '//*[@id="buy-now-button"]').click()
+    time.sleep(5)
 
-    # wait until select address button loads, once after loading click that button
-    #delivery_to_this = wait.until(
-    #    presence_of_element_located((By.XPATH, address_xpath)))
-    #delivery_to_this.click()
+    driver.switch_to.frame("turbo-checkout-iframe")
+    driver.find_element_by_xpath('//*[@id="turbo-checkout-pyo-button"]').click()
 
-    # wait till we get checkout page loaded ,once after loading enter the cvv code and press enter
-    #wait_for_checkout_page = wait.until(
-    #    presence_of_element_located((By.XPATH, '//*[@id="turbo-checkout-pyo-button"]')))
-    #driver.find_element_by_name(
-    #    'addCreditCardVerificationNumber0').send_keys(cvv+Keys.RETURN)
+    time.sleep(15)
 
-    #buy_now_button = driver.find_element_by_xpath('//*[@id="turbo-checkout-pyo-button"]')
-    wait.until(presence_of_element_located((By.XPATH, '//*[@id="turbo-checkout-pyo-button"]')))
-    #buy_now_button.click()
-
-    # at the end click review oreder and we will be redirected to bank page for entering otp
-    #review_order = wait.until(
-    #   presence_of_element_located((By.NAME, 'placeYourOrder1')))
-    #review_order.click()
-
-purchase(secrets.email, secrets.password, 'https://www.amazon.it/dp/B06XNPYZ4Z', secrets.ccv)
+purchase(secrets.email, secrets.password, 'https://www.amazon.fr/dp/B08MPRQR7L', secrets.ccv, "Amazon.fr")
